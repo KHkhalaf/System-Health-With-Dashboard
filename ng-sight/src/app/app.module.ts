a1429f09ab9da012edac5159f3a385506dd0d4dc
hp <hp@DESKTOP-FKTABFC>
2020-10-07 20:40:25 +0300

    add navbar,sidebar components

--- ng-sight/src/app/app.module.ts
+++ ng-sight/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
