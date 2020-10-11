516fb26ad6319208c0b162dde28d6396c8a5692a
hp <hp@DESKTOP-FKTABFC>
2020-10-11 22:40:37 +0300

    update some styles

--- ng-sight/src/app/server/server.component.ts
+++ ng-sight/src/app/server/server.component.ts
@@ -8,11 +8,28 @@ import { Server } from '../shared/server';
 })
 export class ServerComponent implements OnInit {
 
-  @Input() serverInput;
+  @Input() serverInput: Server;
 
+  color:string;
+  buttonText:string;
   constructor() { }
 
   ngOnInit(): void {
+    this.setToggleStatus();
   }
 
+  setToggleStatus(){
+    if(this.serverInput.isOnline){
+      this.color = '#66bb6a';
+      this.buttonText = 'ShutDown';
+    }
+    else{
+      this.color = '#ff6b6b';
+      this.buttonText = 'Start';
+    }
+  }
+  toggleStatus(){
+    this.serverInput.isOnline = !this.serverInput.isOnline;
+    this.setToggleStatus();
+  }
 }
